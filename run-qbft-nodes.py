2533d8aa6b1264a4ce3b7b8f8e6a7eecdd5e65e6
BasharAl-Mohammad <bash.mh.99@gmail.com>
2023-08-09 17:13:14 +0200

    qbft done

--- run-qbft-nodes.py
+++ run-qbft-nodes.py
@@ -1,40 +1,53 @@
-import multiprocessing
-import subprocess
 import os
+import subprocess
+from multiprocessing import Process
 
 def run_command(command):
-    subprocess.run(command, shell=True)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    for line in process.stdout:
+        print(line.strip())
+    process.wait()
+    return process.returncode
+
+def find_enode_url(base_data_path, genesis_file):
+    node_1_data_path = os.path.join(base_data_path, "Node-1", "data")
+    besu_command = f"besu/bin/besu --data-path={node_1_data_path} --genesis-file={genesis_file} --config-file=config/config.toml"
+
+    process_node_1 = subprocess.Popen(besu_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    node_1_enode_url = None
+    for line in process_node_1.stdout:
+        print(line.strip())
+        if "Enode URL" in line:
+            node_1_enode_url = line.split()[-1]
+            print("Found Enode URL:", node_1_enode_url)
+            break
+
+    return node_1_enode_url
 
 if __name__ == "__main__":
-    # Find the number of nodes based on the available folders in QBFT-Network directory
-    qbft_network_path = "QBFT-Network"
-    node_folders = [folder for folder in os.listdir(qbft_network_path) if folder.startswith("Node-")]
-    num_nodes = len(node_folders) - 1
-    processes = []
+    p2p_port=0  
+    base_data_path = "QBFT-Network"
+    genesis_file = os.path.join(base_data_path, "genesis.json")
 
+    node_1_enode_url = find_enode_url(base_data_path, genesis_file)
 
-    # Step 1: Run the first command to capture the enode URL
-    enode_output = []
-    first_command = f"besu/bin/besu --data-path={os.path.join(qbft_network_path, 'Node-1', 'data')} --genesis-file={os.path.join(qbft_network_path, 'genesis.json')} --config-file=config/config.toml"
-    process = multiprocessing.Process(target=run_command, args=(first_command,))
-    processes.append(process)
-    process.start()
+    if node_1_enode_url:
+        command_template = (
+            "besu/bin/besu --data-path={data_path} --genesis-file={genesis_file} "
+            f"--bootnodes={node_1_enode_url} --p2p-port={p2p_port} --rpc-http-enabled "
+            "--rpc-http-api=ETH,NET,QBFT --host-allowlist='*' --rpc-http-cors-origins='all' "
+            "--rpc-http-port={rpc_port}"
+        )
 
+        num_nodes = len([name for name in os.listdir(base_data_path) if os.path.isdir(os.path.join(base_data_path, name))]) - 1
 
-    # List of commands to run in parallel
-    commands = []
-    for i in range(2, num_nodes + 2):  # Assuming node IDs start from 2
-        command = f"besu/bin/besu --data-path={os.path.join(qbft_network_path, f'Node-{i}', 'data')} --genesis-file={os.path.join(qbft_network_path, 'genesis.json')} --bootnodes=enode://751fef3bff526efb44f076ce6c8f4c9e37691c75cc27a8a328a8310979aa4ce89d1e8d698aeb3612d55093640dc427fc99e7c65c1cc0a7dd2d0c98c951586ec2@127.0.0.1:30303 --p2p-port={30303+i} --rpc-http-enabled --rpc-http-api=ETH,NET,QBFT --host-allowlist=\"*\" --rpc-http-cors-origins=\"all\" --rpc-http-port={8547+i}"
-        commands.append(command)
-
-    # Create a process for each command and start them
-    for command in commands:
-        process = multiprocessing.Process(target=run_command, args=(command,))
-        processes.append(process)
-        process.start()
-
-    # Wait for all processes to finish
-    for process in processes:
-        process.join()
-
-    print("All commands have been executed.")
+        # Start processes for Node-2 and subsequent nodes
+        for i in range(2, num_nodes + 2):
+            data_path = os.path.join(base_data_path, f"Node-{i}", "data")
+            p2p_port = 30303 + i
+            rpc_port = 8546 + i
+            command = command_template.format(
+                data_path=data_path, genesis_file=genesis_file, p2p_port=p2p_port, rpc_port=rpc_port
+            )
+            process_node = Process(target=run_command, args=(command,))
+            process_node.start()
